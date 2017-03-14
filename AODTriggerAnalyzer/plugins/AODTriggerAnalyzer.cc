2a0bbe28230d22f243ff9520adae47c43394afe5
Felipe Silva <felipe.silva@cern.ch>
2017-03-14 20:36:17 -0300

    das

--- AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
+++ AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
@@ -110,7 +110,7 @@ AODTriggerAnalyzer::AODTriggerAnalyzer(const edm::ParameterSet& iConfig):
       nEvtsHistosMap["h_L1_"+histoNameSufix] = fs->make<TH1D>( ("h_L1_"+histoNameSufix).c_str() , ("h_L1_"+histoNameSufix+";  Pt (GeV); NEvts").c_str(), 80, 0., 80.);
     }
   }
-  int * v_teste = fs->make<int>();
+  TVectorD * v_teste = fs->make<TVectorD>();
 }
 
 void AODTriggerAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
