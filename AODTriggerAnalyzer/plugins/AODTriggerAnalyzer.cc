d66c9cafdb68ee8d8d4bdcc1b665c9aac8763dc6
Felipe Silva <felipe.silva@cern.ch>
2017-03-15 15:46:44 -0300

    dasda

--- AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
+++ AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
@@ -100,11 +100,11 @@ muonFilterTag_ (iConfig.getParameter<edm::InputTag> ("muonFilterTag")),
 photonFilterTag_ (iConfig.getParameter<edm::InputTag> ("photonFilterTag")),
 
 // HLT Configs
-minPhotonPt_ (iConfig.getUntrackedParameter<double>("minPhotonPt",12.0)),
-minLeadingMuPt_ (iConfig.getUntrackedParameter<double>("minLeadingMuPt",6.0)),
-minTrailMuPt_   (iConfig.getUntrackedParameter<double>("minTrailMuPt",4.0)),                                                                                                                                                                                                                                 
-minDimuonMass_  (iConfig.getUntrackedParameter<double>("minDimuonMass",0.0)),
-maxDimuonMass_  (iConfig.getUntrackedParameter<double>("maxDimuonMass",12.0)),
+minPhotonPt_ (iConfig.getParameter<double>("minPhotonPt",12.0)),
+minLeadingMuPt_ (iConfig.getParameter<double>("minLeadingMuPt",6.0)),
+minTrailMuPt_   (iConfig.getParameter<double>("minTrailMuPt",4.0)),                                                                                                                                                                                                                                 
+minDimuonMass_  (iConfig.getParameter<double>("minDimuonMass",0.0)),
+maxDimuonMass_  (iConfig.getParameter<double>("maxDimuonMass",12.0)),
 
 // L1 Configs    
 configName_ (iConfig.getParameter< std::string > ("configName")),
