9392d6679c5b655f641ae8c3f1767f3f1f67a651
Felipe Silva <felipe.silva@cern.ch>
2017-03-16 21:04:40 -0300

    dasd

--- AODTriggerAnalyzer/test/AODTriggerAnalyzer.py
+++ AODTriggerAnalyzer/test/AODTriggerAnalyzer.py
@@ -56,6 +56,7 @@ process.Zerobias = cms.EDAnalyzer("AODTriggerAnalyzer",
     triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
     muonFilterTag = cms.InputTag ("hltDoubleMuon0L3PreFiltered0","","HLT"),
     photonFilterTag = cms.InputTag ("hltEG22HEFilter","","HLT"),
+    photonFilterTag = cms.InputTag ("hltEGL1SingleEG18Filter","","HLT"),
 )
 
 process.effana1 = process.Zerobias.clone(
