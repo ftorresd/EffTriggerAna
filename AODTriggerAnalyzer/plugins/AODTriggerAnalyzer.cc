d89210b2a50bb80ba200df9f3843642fc646502a
Felipe Silva <felipe.silva@cern.ch>
2017-03-19 11:15:45 -0300

    sdasdsa

--- AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
+++ AODTriggerAnalyzer/plugins/AODTriggerAnalyzer.cc
@@ -587,10 +587,10 @@ AODTriggerAnalyzer::endJob()
 
     // set evts counters
   // TnEvts->SetElements(nEvts, nEvtsRECO, nEvtsHLT, nEvtsHLTRECO)
-  TnEvts(0) = nEvts;
-  TnEvts(1) = nEvtsRECO;
-  TnEvts(2) = nEvtsHLT;
-  TnEvts(3) = nEvtsHLTRECO;
+  TnEvts[0] = nEvts;
+  TnEvts[1] = nEvtsRECO;
+  TnEvts[2] = nEvtsHLT;
+  TnEvts[3] = nEvtsHLTRECO;
 
   std::cout << TnEvts->Print() << std::endl;
   
