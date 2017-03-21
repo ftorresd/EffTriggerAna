bf14b70247a8b308aea44dbfd78679095c311c9d
Felipe Silva <felipe.silva@cern.ch>
2017-03-20 21:25:27 -0300

    dasdsa

--- AODTriggerAnalyzer/macros/effTriggerPlotter.py
+++ AODTriggerAnalyzer/macros/effTriggerPlotter.py
@@ -10,12 +10,24 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(1)
 ROOT.TH1.SetDefaultSumw2(True)
 ROOT.gStyle.SetOptStat(0)
-
-effFiles = {
-	'DYJetsToLL_M-1to10' : ROOT.TFile('efficiency_DYJetsToLL_M-1to10.root'),
-	# 'DYJetsToLL_M-10to50' : ROOT.TFile('efficiency_DYJetsToLL_M-10to50.root'),
-	# 'DYJetsToLL_M-50' : ROOT.TFile('efficiency_DYJetsToLL_M-50.root'),
-	'ZToJPsiGamma' : ROOT.TFile('efficiency_ZToJPsiGamma.root')
+	
+effFilesXSec = {
+	'QCD_MuEnrichedPt5' : [
+				(ROOT.TFile('efficiency_QCD_Pt-15to20_MuEnrichedPt5'), 1.0),
+				(ROOT.TFile('efficiency_QCD_Pt-20to30_MuEnrichedPt5'), 2.0),
+				(ROOT.TFile('efficiency_QCD_Pt-30to50_MuEnrichedPt5'), 2.0),
+				(ROOT.TFile('efficiency_QCD_Pt-50to80_MuEnrichedPt5'), 2.0),
+				(ROOT.TFile('efficiency_QCD_Pt-80to120_MuEnrichedPt5'), 2.0),
+				(ROOT.TFile('efficiency_QCD_Pt-120to170_MuEnrichedPt5'), 2.0)
+				],
+	'DYJetsToLL' : [
+				(ROOT.TFile('efficiency_DYJetsToLL_M-1to10.root'), 1.0),
+				(ROOT.TFile('efficiency_DYJetsToLL_M-10to50.root'), 2.0),
+				(ROOT.TFile('efficiency_DYJetsToLL_M-50.root'), 3.0)
+				],
+	'ZToJPsiGamma' : [
+				(ROOT.TFile('efficiency_ZToJPsiGamma.root'), 1.0)
+				]
 }
 
 configNames = [
@@ -50,22 +62,26 @@ def translateConfigNameToLegend(config, egCut):
 		return "DoubleMu_X_OS_IsoEG_"+str(egCut)
 
 
-def plotEff(file, configNames, egCut, selectionSequence):
+def plotEff(dataset, filesXSec, configNames, egCut, selectionSequence):
 	ROOT.gROOT.Reset()
 	c1 = ROOT.TCanvas("c1","c1",200,10,1050,750);
 	leg = ROOT.TLegend(0.9-.38,0.7,0.9,0.9);
 
 	for index, config in enumerate(configNames):
-		histo = effFiles[file].Get(config).Get("h_L1"+selectionSequence+"_"+config+"_EG_"+str(egCut))
-		histo.Scale( 1./(effFiles[file].Get(config).Get("h_nEvts"+selectionSequence+"_"+config).GetBinContent(1) ) )
-		histo.SetTitle("")
-		histo.SetMarkerStyle(24)
-		histo.SetMarkerColor(index+1)
-		histo.SetMinimum(0.0);
-		histo.SetMaximum(1.15);
-		histo.GetXaxis().SetRangeUser(0.0, 40.0)
-  		leg.AddEntry(histo,translateConfigNameToLegend(config, egCut));
-		histo.Draw("same")
+		histoToPlot = filesXSec[0][0].Get(config).Get("h_L1"+selectionSequence+"_"+config+"_EG_"+str(egCut))
+		histoToPlot.Scale( (filesXSec[0][1]/filesXSec[0][0].Get(config).Get("h_nEvts_"+config).GetBinContent(1))/(filesXSec[0][0].Get(config).Get("h_nEvts"+selectionSequence+"_"+config).GetBinContent(1) ) )
+		for fileXSec in filesXSec[1:]:
+			histo = fileXSec[0].Get(config).Get("h_L1"+selectionSequence+"_"+config+"_EG_"+str(egCut))
+			histo.Scale( (fileXSec[1]/fileXSec[0].Get(config).Get("h_nEvts_"+config).GetBinContent(1))/(fileXSec[0].Get(config).Get("h_nEvts"+selectionSequence+"_"+config).GetBinContent(1) ) )
+			histoToPlot.Add(histo)
+		histoToPlot.SetTitle("")
+		histoToPlot.SetMarkerStyle(24)
+		histoToPlot.SetMarkerColor(index+1)
+		histoToPlot.SetMinimum(0.0);
+		histoToPlot.SetMaximum(1.15);
+		histoToPlot.GetXaxis().SetRangeUser(0.0, 40.0)
+  		leg.AddEntry(histoToPlot,translateConfigNameToLegend(config, egCut));
+		histoToPlot.Draw("same")
 		# print "oi"
 		# histo.Print()
  
@@ -75,15 +91,15 @@ def plotEff(file, configNames, egCut, selectionSequence):
 	# leg->AddEntry("gr","Graph with error bars","lep");
 	leg.Draw();
 	c1.Update()
-	c1.SaveAs("l1Plots/"+file+"/L1"+selectionSequence+"/h_L1"+selectionSequence+"_EG_"+str(egCut)+".pdf");
-	c1.SaveAs("l1Plots/"+file+"/L1"+selectionSequence+"/h_L1"+selectionSequence+"_EG_"+str(egCut)+".png");
+	c1.SaveAs("l1Plots/"+dataset+"/L1"+selectionSequence+"/h_L1"+selectionSequence+"_EG_"+str(egCut)+".pdf");
+	c1.SaveAs("l1Plots/"+dataset+"/L1"+selectionSequence+"/h_L1"+selectionSequence+"_EG_"+str(egCut)+".png");
 
 # loop over datasets file
 os.system("rm -rf l1Plots ; mkdir l1Plots")
-for file_ in effFiles:
-	os.system("mkdir l1Plots/"+file_)
+for dataset in effFilesXSec:
+	os.system("mkdir l1Plots/"+dataset)
 	for selectionSequence in selectionSequences:
-		os.system("mkdir l1Plots/"+file_+"/L1"+selectionSequence)
+		os.system("mkdir l1Plots/"+dataset+"/L1"+selectionSequence)
 		for egCut in range(51):
-			plotEff(file_, configNames, egCut, selectionSequence)
+			plotEff(dataset, effFilesXSec[dataset], configNames, egCut, selectionSequence)
 
