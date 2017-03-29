85e71177bfff0338b79756c799fe008ebfae2ca7
Felipe Silva <felipe.silva@cern.ch>
2017-03-29 19:51:19 -0300

    dsada

--- AODTriggerAnalyzer/macros/effTriggerPlotter.py
+++ AODTriggerAnalyzer/macros/effTriggerPlotter.py
@@ -5,6 +5,7 @@ Plotter for EffTriggerAna
 """
 
 import ROOT, os
+import csv
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(1)
@@ -85,153 +86,10 @@ selectionSequences = [
 
 # needs to be rewriten
 def translateConfigNameToLegend(config, egCut):
-	if (config == "DoubleMu_X"):
-		return "DoubleMu_X"
-	if (config == "DoubleMu_X_OS"):
-		return "DoubleMu_X_OS"		
-	if (config == "DoubleMu_X_EG_Y"):
-		return "DoubleMu_X_EG_"+str(egCut)
-	if (config == "DoubleMu_X_OS_EG_Y"):
-		return "DoubleMu_X_OS_EG_"+str(egCut)
-	if (config == "DoubleMu_X_IsoEG_Y"):
-		return "DoubleMu_X_IsoEG_"+str(egCut)
-	if (config == "DoubleMu_X_OS_IsoEG_Y"):
-		return "DoubleMu_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_3_X"):
-		return "Mu_3_X"
-	if (config == "Mu_3_X_OS"):
-		return "Mu_3_X_OS"		
-	if (config == "Mu_3_X_EG_Y"):
-		return "Mu_3_X_EG_"+str(egCut)
-	if (config == "Mu_3_X_OS_EG_Y"):
-		return "Mu_3_X_OS_EG_"+str(egCut)
-	if (config == "Mu_3_X_IsoEG_Y"):
-		return "Mu_3_X_IsoEG_"+str(egCut)
-	if (config == "Mu_3_X_OS_IsoEG_Y"):
-		return "Mu_3_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_4_X"):
-		return "Mu_4_X"
-	if (config == "Mu_4_X_OS"):
-		return "Mu_4_X_OS"		
-	if (config == "Mu_4_X_EG_Y"):
-		return "Mu_4_X_EG_"+str(egCut)
-	if (config == "Mu_4_X_OS_EG_Y"):
-		return "Mu_4_X_OS_EG_"+str(egCut)
-	if (config == "Mu_4_X_IsoEG_Y"):
-		return "Mu_4_X_IsoEG_"+str(egCut)
-	if (config == "Mu_4_X_OS_IsoEG_Y"):
-		return "Mu_4_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_5_X"):
-		return "Mu_5_X"
-	if (config == "Mu_5_X_OS"):
-		return "Mu_5_X_OS"		
-	if (config == "Mu_5_X_EG_Y"):
-		return "Mu_5_X_EG_"+str(egCut)
-	if (config == "Mu_5_X_OS_EG_Y"):
-		return "Mu_5_X_OS_EG_"+str(egCut)
-	if (config == "Mu_5_X_IsoEG_Y"):
-		return "Mu_5_X_IsoEG_"+str(egCut)
-	if (config == "Mu_5_X_OS_IsoEG_Y"):
-		return "Mu_5_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_6_X"):
-		return "Mu_6_X"
-	if (config == "Mu_6_X_OS"):
-		return "Mu_6_X_OS"		
-	if (config == "Mu_6_X_EG_Y"):
-		return "Mu_6_X_EG_"+str(egCut)
-	if (config == "Mu_6_X_OS_EG_Y"):
-		return "Mu_6_X_OS_EG_"+str(egCut)
-	if (config == "Mu_6_X_IsoEG_Y"):
-		return "Mu_6_X_IsoEG_"+str(egCut)
-	if (config == "Mu_6_X_OS_IsoEG_Y"):
-		return "Mu_6_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_7_X"):
-		return "Mu_7_X"
-	if (config == "Mu_7_X_OS"):
-		return "Mu_7_X_OS"		
-	if (config == "Mu_7_X_EG_Y"):
-		return "Mu_7_X_EG_"+str(egCut)
-	if (config == "Mu_7_X_OS_EG_Y"):
-		return "Mu_7_X_OS_EG_"+str(egCut)
-	if (config == "Mu_7_X_IsoEG_Y"):
-		return "Mu_7_X_IsoEG_"+str(egCut)
-	if (config == "Mu_7_X_OS_IsoEG_Y"):
-		return "Mu_7_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_8_X"):
-		return "Mu_8_X"
-	if (config == "Mu_8_X_OS"):
-		return "Mu_8_X_OS"		
-	if (config == "Mu_8_X_EG_Y"):
-		return "Mu_8_X_EG_"+str(egCut)
-	if (config == "Mu_8_X_OS_EG_Y"):
-		return "Mu_8_X_OS_EG_"+str(egCut)
-	if (config == "Mu_8_X_IsoEG_Y"):
-		return "Mu_8_X_IsoEG_"+str(egCut)
-	if (config == "Mu_8_X_OS_IsoEG_Y"):
-		return "Mu_8_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_9_X"):
-		return "Mu_9_X"
-	if (config == "Mu_9_X_OS"):
-		return "Mu_9_X_OS"		
-	if (config == "Mu_9_X_EG_Y"):
-		return "Mu_9_X_EG_"+str(egCut)
-	if (config == "Mu_9_X_OS_EG_Y"):
-		return "Mu_9_X_OS_EG_"+str(egCut)
-	if (config == "Mu_9_X_IsoEG_Y"):
-		return "Mu_9_X_IsoEG_"+str(egCut)
-	if (config == "Mu_9_X_OS_IsoEG_Y"):
-		return "Mu_9_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_10_X"):
-		return "Mu_10_X"
-	if (config == "Mu_10_X_OS"):
-		return "Mu_10_X_OS"		
-	if (config == "Mu_10_X_EG_Y"):
-		return "Mu_10_X_EG_"+str(egCut)
-	if (config == "Mu_10_X_OS_EG_Y"):
-		return "Mu_10_X_OS_EG_"+str(egCut)
-	if (config == "Mu_10_X_IsoEG_Y"):
-		return "Mu_10_X_IsoEG_"+str(egCut)
-	if (config == "Mu_10_X_OS_IsoEG_Y"):
-		return "Mu_10_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_11_X"):
-		return "Mu_11_X"
-	if (config == "Mu_11_X_OS"):
-		return "Mu_11_X_OS"		
-	if (config == "Mu_11_X_EG_Y"):
-		return "Mu_11_X_EG_"+str(egCut)
-	if (config == "Mu_11_X_OS_EG_Y"):
-		return "Mu_11_X_OS_EG_"+str(egCut)
-	if (config == "Mu_11_X_IsoEG_Y"):
-		return "Mu_11_X_IsoEG_"+str(egCut)
-	if (config == "Mu_11_X_OS_IsoEG_Y"):
-		return "Mu_11_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_12_X"):
-		return "Mu_12_X"
-	if (config == "Mu_12_X_OS"):
-		return "Mu_12_X_OS"		
-	if (config == "Mu_12_X_EG_Y"):
-		return "Mu_12_X_EG_"+str(egCut)
-	if (config == "Mu_12_X_OS_EG_Y"):
-		return "Mu_12_X_OS_EG_"+str(egCut)
-	if (config == "Mu_12_X_IsoEG_Y"):
-		return "Mu_12_X_IsoEG_"+str(egCut)
-	if (config == "Mu_12_X_OS_IsoEG_Y"):
-		return "Mu_12_X_OS_IsoEG_"+str(egCut)
-	if (config == "Mu_13_X"):
-		return "Mu_13_X"
-	if (config == "Mu_13_X_OS"):
-		return "Mu_13_X_OS"		
-	if (config == "Mu_13_X_EG_Y"):
-		return "Mu_13_X_EG_"+str(egCut)
-	if (config == "Mu_13_X_OS_EG_Y"):
-		return "Mu_13_X_OS_EG_"+str(egCut)
-	if (config == "Mu_13_X_IsoEG_Y"):
-		return "Mu_13_X_IsoEG_"+str(egCut)
-	if (config == "Mu_13_X_OS_IsoEG_Y"):
-		return "Mu_13_X_OS_IsoEG_"+str(egCut)
+	return config.replace("Y",str(egCut))
 
 
-def plotEff(dataset, filesXSec, configNames, egCut, selectionSequence):
+def plotEff(dataset, filesXSec, configNames, egCut, selectionSequence, writer):
 	ROOT.gROOT.Reset()
 	c1 = ROOT.TCanvas("c1","c1",200,10,1050,750);
 	leg = ROOT.TLegend(0.9-.38,0.7,0.9,0.9);
@@ -256,7 +114,12 @@ def plotEff(dataset, filesXSec, configNames, egCut, selectionSequence):
 			histoToPlot.SetMaximum(1.15);
 			histoToPlot.GetXaxis().SetRangeUser(0.0, 40.0)
 	  		leg.AddEntry(histoToPlot,translateConfigNameToLegend(config, egCut));
-			histoToPlot.Draw("same")
+	  		# histoToPlot.SetBarOffset(0.4)
+			# histoToPlot.Draw("SAME TEXT45")
+			histoToPlot.Draw("SAME")
+			# fill csv
+			if (9 <= egCut <= 12):
+				writer.writerow([egCut, translateConfigNameToLegend(config, egCut), histoToPlot.GetBinContent(4+1), histoToPlot.GetBinContent(5+1), histoToPlot.GetBinContent(6+1), histoToPlot.GetBinContent(7+1), ])
 
 	leg.Draw();
 	c1.Update()
@@ -272,8 +135,12 @@ for dataset in effFilesXSec:
 		for configName in configSets:
 			os.system("mkdir l1Plots/"+dataset+"/L1"+selectionSequence+"/"+configName)
 			# for egCut in range(51):
-			for egCut in [0,5,6,7,8,9,10,11,12,13,14,15]:
-				plotEff(dataset, effFilesXSec[dataset], configSets[configName], egCut, selectionSequence)
+			effCSVFile  = open("l1Plots/"+dataset+"/L1"+selectionSequence+"/"+configName+"/h_L1"+selectionSequence+".csv", "wb")
+			writer = csv.writer(effCSVFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			writer.writerow(["EG Cut","L1 Config", "DoubleMu_"+str(4), "DoubleMu_"+str(5), "DoubleMu_"+str(6), "DoubleMu_"+str(7)])
+			for egCut in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
+				plotEff(dataset, effFilesXSec[dataset], configSets[configName], egCut, selectionSequence, writer)
+			effCSVFile.close()
 
 
 				
